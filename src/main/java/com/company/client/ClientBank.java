713880a497ecd148804ad0ebd73fb90b2eab5829
asurovehina <olesia.surovehina@globallogic.com>
2019-09-09 00:08:51 +0200

    finally completed

--- src/main/java/com/company/client/ClientBank.java
+++ src/main/java/com/company/client/ClientBank.java
@@ -2,21 +2,21 @@ package com.company.client;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.testng.annotations.NoInjection;
-import org.testng.annotations.Test;
 
-@Test
+
+
 public class ClientBank extends ClientLogin {
     public String bankName;
     public void BankCreation () throws InterruptedException {
         ChromeConfig();
-        driver.get(ClientDev1);
-        driver.manage().window().maximize();
-        driver.findElement(By.xpath("//*[@id=\"a11y-realm\"]")).sendKeys("alesiacheck");
-        driver.findElement(By.xpath("//*[@id=\"a11y-username\"]")).sendKeys("autouse");
-        driver.findElement(By.xpath("//*[@id=\"a11y-password\"]")).sendKeys("Password1");
-        driver.findElement(By.xpath("//*[@id=\"login-action-submit\"]/span")).click();
-        Thread.sleep(2000);//login first time
+//        driver.get(ClientDev1);
+//        driver.manage().window().maximize();
+//        driver.findElement(By.xpath("//*[@id=\"a11y-realm\"]")).sendKeys("alesiacheck");
+//        driver.findElement(By.xpath("//*[@id=\"a11y-username\"]")).sendKeys("autouse");
+//        driver.findElement(By.xpath("//*[@id=\"a11y-password\"]")).sendKeys("Password1");
+//        driver.findElement(By.xpath("//*[@id=\"login-action-submit\"]/span")).click();
+//        Thread.sleep(2000);//login first time`
+        ClientUserLogin();
         driver.findElement(By.xpath("//*[@id=\"gmp-ADMINSTRATION\"]")).click();
         driver.findElement(By.xpath("//*[@id=\"nav\"]/div/header/div/div[2]/div/nav/ul/li[5]/ul/li[3]/a")).click();
         Thread.sleep(2000);
@@ -32,22 +32,22 @@ public class ClientBank extends ClientLogin {
         driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/form/div[2]/div/button[1]")).click();
         Thread.sleep(2000);
     }
-    public void CreateAccount () throws InterruptedException {
+    public void CreateAccount (int accountNumber) throws InterruptedException {
         driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div/ul/li[2]/a/span")).click(); //go to tab accounts
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div/div/div/div[1]/button")).click(); //add new account
-        driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div/div/div[2]/div/div[2]/form/div/div/div/div/div/span/div[1]/div/ul[1]/li/span")).click(); //open the bank dropdown
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#main > div.bank-and-account-maintenance > div > div > div.modal > div > div.modal-body > form > div > div > div > div > div > span > div.selected > div > ul.selected-items > li > span")).click(); //open the bank dropdown
         driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div/div/div[2]/div/div[2]/form/div/div/div/div/div/div/input")).sendKeys(bankName);
         driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div/div/div[2]/div/div[2]/form/div/div/div/div/div/div/input")).sendKeys(Keys.ENTER);
         driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div/div/div[2]/div/div[3]/button[1]")).click();
         Thread.sleep(2000);
-         int accountNumber = 010203040;
-        driver.findElement(By.xpath("//*[@id=\"input-10\"]")).sendKeys(String.valueOf(accountNumber));
+        driver.findElement(By.xpath("//input[contains(@name,'ACCOUNTNUM')]")).sendKeys(String.valueOf(accountNumber));
         String accName = getSaltString();
-        driver.findElement(By.xpath("//*[@id=\"input-12\"]")).sendKeys(accName);
-        driver.findElement(By.xpath("//*[@id=\"input-29\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"input-24\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"main\"]/div[3]/form/div[2]/div/button[1]")).click();
+        driver.findElement(By.xpath("//input[@name='ACCOUNTNAME']")).sendKeys(accName);
+        driver.findElement(By.xpath("//input[contains(@name,'CODE=RTGS&SUBCODE=TRANSFER')]")).click();
+        driver.findElement(By.xpath("//input[contains(@name,'CODE=RTGS&SUBCODE=ITRNSFER')]")).click();
+        driver.findElement(By.xpath("//button[@type='submit'][contains(.,'Save')]")).click();
         Thread.sleep(2000);
     }
 }
---
+++ src/main/java/com/tests/TransferOneToOneTest.java
@@ -0,0 +1,58 @@
+package com.tests;
+
+import com.company.client.ClientBank;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TransferOneToOneTest extends ClientBank {
+    @BeforeMethod
+    public void setupData () throws InterruptedException {
+        BankCreation();
+        CreateAccount(123456789);
+        CreateAccount(122345678);
+        CreateAccount(113456789);
+        CreateAccount(123356789);
+    }
+    @Test
+    public void TransferCreation () throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"gmp-PAYMENTS\"]")).click(); //click the payment & transfers
+        driver.findElement(By.xpath("//*[@id=\"nav\"]/div/header/div/div[2]/div/nav/ul/li[3]/ul/li[3]/a")).click(); //go to transfer management
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"TRANSFERS\"]/div")).click();
+        String urlOneToOne = "https://pcm-dev1-fi9001-web01.saas-n.com/ui/PAYMENTS/addTransfer/ONE2ONE";
+        driver.get(urlOneToOne);
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#select2-chosen-1")).click(); //'from account' drop-down
+        driver.findElement(By.cssSelector("#s2id_autogen1_search")).sendKeys("123456789");
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#s2id_autogen1_search")).sendKeys(Keys.ENTER);
+        driver.findElement(By.cssSelector("#select2-chosen-2")).click(); //'to account' drop-down
+        driver.findElement(By.cssSelector("#s2id_autogen2_search")).sendKeys("122345678");
+        Thread.sleep(2000);//
+        driver.findElement(By.cssSelector("#s2id_autogen2_search")).sendKeys(Keys.ENTER);
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[contains(@class,'form-control text-right')]")).sendKeys("100");
+        driver.findElement(By.xpath("//*[@id=\"main\"]/div/div/section/div/div[5]/div/button")).click();
+        Thread.sleep(2000);
+        //adding the second one transfer
+        driver.findElement(By.cssSelector("#select2-chosen-6")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#s2id_autogen6_search")).sendKeys("113456789");
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#s2id_autogen6_search")).sendKeys(Keys.ENTER);
+        driver.findElement(By.cssSelector("#select2-chosen-5")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#s2id_autogen5_search")).sendKeys("123356789");
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#s2id_autogen5_search")).sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("(//input[contains(@class,'form-control text-right')])[2]")).sendKeys("110");
+        driver.findElement(By.xpath("//button[@type='button'][contains(.,'Submit')]")).click();
+    }
+    @AfterMethod
+    public void after () {
+        driver.quit();
+    }
+}
