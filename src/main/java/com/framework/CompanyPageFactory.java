c7e0b54c6ae6b2c7a1388ddfd2adf78ab68a5d6f
asurovehina <olesia.surovehina@globallogic.com>
2019-09-09 23:11:34 +0200

    Company page factory filled

--- src/main/java/com/framework/CompanyPageFactory.java
+++ src/main/java/com/framework/CompanyPageFactory.java
@@ -1,4 +1,18 @@
 package com.framework;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class CompanyPageFactory {
+    @FindBy(xpath = "//*[@id=\"modifyUserGroupDetail\"]/span")
+    WebElement ClientCompanyModify;
+
+    @FindBy(xpath = "//*[@id=\"modifyUserGroupEntitlements\"]/span")
+    WebElement ClientEntitlements;
+        public void ClickClientCompanyModify () {
+        ClientCompanyModify.click();
+    }
+        public void ClickClientEntitlements () {
+        ClientEntitlements.click();
+    }
 }
