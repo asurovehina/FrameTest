8f6d4957adbdbac006e2fea69922eea8b32e6af4
asurovehina <olesia.surovehina@globallogic.com>
2019-09-09 23:38:47 +0200

    MenuTab factory filled

--- src/main/java/com/framework/MenuTab.java
+++ src/main/java/com/framework/MenuTab.java
@@ -7,9 +7,13 @@ import org.openqa.selenium.WebElement;
 public class MenuTab {
     public static WebElement element = null;
 
-    public static void ClickCompany(WebDriver driver) {
+    public static WebElement CompanyEl (WebDriver driver) {
         element = driver.findElement(By.xpath("//a[contains(@data-bt-itemrequestparams,'filterID=15151')]"));
-        element.click();
+        return element;
     }
+//    public static void ClickCompanyEl(WebDriver driver) {
+//        element = driver.findElement(By.xpath("//a[contains(@data-bt-itemrequestparams,'filterID=15151')]"));
+//        element.click();
+//    }
     // *Could be another options e.g. Client Role, Client User etc.*
 }
--- src/main/java/com/framework/MenuTabFactory.java
+++ src/main/java/com/framework/MenuTabFactory.java
@@ -1,4 +1,13 @@
 package com.framework;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class MenuTabFactory {
+    @FindBy(xpath ="//a[contains(@data-bt-itemrequestparams,'filterID=15151')]")
+    WebElement CompanyEl;
+
+    public void ClickCompanyEl() {
+        CompanyEl.click();
+    }
 }
