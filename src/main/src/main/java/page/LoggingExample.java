e8c37b23a84025546ccaea2e22d223383977b219
asurovehina <olesia.surovehina@globallogic.com>
2019-12-09 14:49:51 +0100

    dependency to Maven is added

--- src/main/src/main/java/page/LoggingExample.java
+++ src/main/src/main/java/page/LoggingExample.java
@@ -4,12 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LoggingExample {
-    private static final Logger log = LogManager.getLogger(LoggingConsole.class.getName());
-
-        public static void main(String[] args) {
+    private static final Logger log = LogManager.getLogger(LoggingExample.class.getName());
+    public static void main(String[] args) {
             log.debug("Debug Message Logged");
             log.error("Error Message Logged");
             log.fatal("Fatal Message Logged");
         }
     }
-}
+
